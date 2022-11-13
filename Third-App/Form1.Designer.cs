867602e04c0a4eba26cc7ecaf4656fd7b30d0a78
MuhammadDaffa <muhammaddaffa0606@gmail.com>
2022-11-13 21:30:36 +0700

    add_ModbusTCP

--- Third-App/Form1.Designer.cs
+++ Third-App/Form1.Designer.cs
@@ -28,13 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.panelMenu = new System.Windows.Forms.FlowLayoutPanel();
             this.panelLogo = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
             this.buttonDashboard = new System.Windows.Forms.Button();
             this.buttonReporting = new System.Windows.Forms.Button();
             this.buttonLogging = new System.Windows.Forms.Button();
             this.buttonSetting = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            this.panelTopBar = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.panelMenu.SuspendLayout();
             this.panelLogo.SuspendLayout();
             this.SuspendLayout();
@@ -49,18 +54,31 @@
             this.panelMenu.Controls.Add(this.buttonSetting);
             this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
             this.panelMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMenu.Margin = new System.Windows.Forms.Padding(0);
             this.panelMenu.Name = "panelMenu";
-            this.panelMenu.Size = new System.Drawing.Size(205, 840);
+            this.panelMenu.Size = new System.Drawing.Size(199, 840);
             this.panelMenu.TabIndex = 0;
             // 
             // panelLogo
             // 
             this.panelLogo.Controls.Add(this.label1);
             this.panelLogo.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panelLogo.Location = new System.Drawing.Point(3, 3);
+            this.panelLogo.Location = new System.Drawing.Point(0, 0);
+            this.panelLogo.Margin = new System.Windows.Forms.Padding(0);
             this.panelLogo.Name = "panelLogo";
             this.panelLogo.Size = new System.Drawing.Size(200, 74);
             this.panelLogo.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(13, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(169, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Daffa\'s IoT Project";
             // 
             // buttonDashboard
             // 
@@ -72,7 +90,8 @@
             this.buttonDashboard.ForeColor = System.Drawing.Color.Transparent;
             this.buttonDashboard.Image = global::Third_App.Properties.Resources.view_dashboard_outline;
             this.buttonDashboard.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.buttonDashboard.Location = new System.Drawing.Point(3, 83);
+            this.buttonDashboard.Location = new System.Drawing.Point(0, 74);
+            this.buttonDashboard.Margin = new System.Windows.Forms.Padding(0);
             this.buttonDashboard.Name = "buttonDashboard";
             this.buttonDashboard.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
             this.buttonDashboard.Size = new System.Drawing.Size(200, 64);
@@ -90,7 +109,8 @@
             this.buttonReporting.ForeColor = System.Drawing.Color.Transparent;
             this.buttonReporting.Image = global::Third_App.Properties.Resources.file_chart;
             this.buttonReporting.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.buttonReporting.Location = new System.Drawing.Point(3, 153);
+            this.buttonReporting.Location = new System.Drawing.Point(0, 138);
+            this.buttonReporting.Margin = new System.Windows.Forms.Padding(0);
             this.buttonReporting.Name = "buttonReporting";
             this.buttonReporting.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
             this.buttonReporting.Size = new System.Drawing.Size(200, 64);
@@ -108,7 +128,8 @@
             this.buttonLogging.ForeColor = System.Drawing.Color.Transparent;
             this.buttonLogging.Image = global::Third_App.Properties.Resources.clipboard_text_clock;
             this.buttonLogging.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.buttonLogging.Location = new System.Drawing.Point(3, 223);
+            this.buttonLogging.Location = new System.Drawing.Point(0, 202);
+            this.buttonLogging.Margin = new System.Windows.Forms.Padding(0);
             this.buttonLogging.Name = "buttonLogging";
             this.buttonLogging.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
             this.buttonLogging.Size = new System.Drawing.Size(200, 64);
@@ -126,7 +147,8 @@
             this.buttonSetting.ForeColor = System.Drawing.Color.Transparent;
             this.buttonSetting.Image = global::Third_App.Properties.Resources.cog_outline;
             this.buttonSetting.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.buttonSetting.Location = new System.Drawing.Point(3, 293);
+            this.buttonSetting.Location = new System.Drawing.Point(0, 266);
+            this.buttonSetting.Margin = new System.Windows.Forms.Padding(0);
             this.buttonSetting.Name = "buttonSetting";
             this.buttonSetting.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
             this.buttonSetting.Size = new System.Drawing.Size(200, 64);
@@ -135,16 +157,41 @@
             this.buttonSetting.UseVisualStyleBackColor = true;
             this.buttonSetting.Click += new System.EventHandler(this.buttonSetting_Click);
             // 
-            // label1
+            // panelTopBar
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.Transparent;
-            this.label1.Location = new System.Drawing.Point(13, 24);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(169, 26);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Daffa\'s IoT Project";
+            this.panelTopBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTopBar.Location = new System.Drawing.Point(199, 0);
+            this.panelTopBar.Name = "panelTopBar";
+            this.panelTopBar.Size = new System.Drawing.Size(1055, 29);
+            this.panelTopBar.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(526, 216);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(299, 108);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Transparent;
+            this.label3.Location = new System.Drawing.Point(865, 235);
+            this.label3.Name = "label3";
+            this.label3.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label3.Size = new System.Drawing.Size(104, 73);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "°C";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
@@ -152,13 +199,18 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Indigo;
             this.ClientSize = new System.Drawing.Size(1254, 840);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panelTopBar);
             this.Controls.Add(this.panelMenu);
             this.Name = "Form1";
             this.Text = " ";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.panelMenu.ResumeLayout(false);
             this.panelLogo.ResumeLayout(false);
             this.panelLogo.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -171,6 +223,10 @@
         private System.Windows.Forms.Button buttonLogging;
         private System.Windows.Forms.Button buttonSetting;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panelTopBar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- Third-App/Form1.cs
+++ Third-App/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EasyModbus;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,6 +9,7 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace Third_App
 {
@@ -18,16 +20,13 @@ namespace Third_App
         private Button currentButton;
         private Random random;
         private int tempIndex;
+        private ModbusClient modbusClient;
 
         public Form1()
         {
             InitializeComponent();
             random = new Random();
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
+            timer1.Start();
         }
 
         //Methods
@@ -57,7 +56,7 @@ namespace Third_App
                     currentButton.BackColor = color;
                     currentButton.ForeColor = Color.White;
                     currentButton.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-                    panelTitleBar.BackColor = color;
+                    panelTopBar.BackColor = color;
                     panelLogo.BackColor = ThemeColors.ChangeColorBrightness(color, -0.3);
                 }
             }
@@ -101,6 +100,39 @@ namespace Third_App
             ActiveButton(sender);
             //label2.Text = "Setting";
 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void ComModbusTCP()
+        {
+            modbusClient = new ModbusClient("192.168.1.6", 502);    //Ip-Address and Port of Modbus-TCP-Server
+            modbusClient.Connect();                                                    //Connect to Server
+            //modbusClient.WriteMultipleCoils(4, new bool[] { true, true, true, true, true, true, true, true, true, true });    //Write Coils starting with Address 5
+            //bool[] readCoils = modbusClient.ReadCoils(9, 10);                        //Read 10 Coils from Server, starting with address 10
+            int[] readInputRegisters = modbusClient.ReadInputRegisters(0, 1);    //Read 10 Holding Registers from Server, starting with Address 1
+
+            // Console Output
+            // for (int i = 0; i < readCoils.Length; i++)
+            //    Console.WriteLine("Value of Coil " + (9 + i + 1) + " " + readCoils[i].ToString());
+
+            for (int i = 0; i < readInputRegisters.Length; i++)
+            {
+                Console.WriteLine("Value of Input Register " + (i + 1) + " " + readInputRegisters[i].ToString());
+                label2.Text = readInputRegisters[i].ToString();
+            }
+
+            //Disconnect from Server
+            modbusClient.Disconnect();                                                
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ComModbusTCP();
         }
     }
 }
